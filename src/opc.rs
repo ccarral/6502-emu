a726df62057de6a998c72a4bc612bb879c57a873
Carlos Carral C <carloscarral13@gmail.com>
2022-02-16 09:51:16 -0600

    refactor: move opc array decl to const fn.

--- src/opc.rs
+++ src/opc.rs
@@ -1,26 +1,21 @@
 #[derive(Copy, Clone)]
 pub struct OpMode(pub Inst, pub AddressMode, pub u8);
 
-const OPS: [Option<OpMode>; 0x10] = [
-    // 0x00
-    Some(OpMode(Inst::BRK, AddressMode::IMPL, 0)),
-    Some(OpMode(Inst::Ora, AddressMode::INDX, 6)),
-    None,
-    None,
-    None,
-    Some(OpMode(Inst::Ora, AddressMode::ZPG, 3)),
-    Some(OpMode(Inst::ASL, AddressMode::ZPG, 0)),
-    None,
-    Some(OpMode(Inst::Php, AddressMode::IMPL, 0)),
-    Some(OpMode(Inst::Ora, AddressMode::IMM, 2)),
-    Some(OpMode(Inst::ASL, AddressMode::ACC, 0)),
-    None,
-    None,
-    Some(OpMode(Inst::Ora, AddressMode::ABS, 4)),
-    Some(OpMode(Inst::ASL, AddressMode::ABS, 0)),
-    None,
-    // 0x10
-];
+pub const fn init_opc_array() -> [Option<OpMode>; 0xFF] {
+    let mut opc_arr: [Option<OpMode>; 0xFF] = [None; 0xFF];
+    opc_arr[0x00] = Some(OpMode(Inst::BRK, AddressMode::IMPL, 0));
+    opc_arr[0x01] = Some(OpMode(Inst::Ora, AddressMode::INDX, 6));
+    opc_arr[0x05] = Some(OpMode(Inst::Ora, AddressMode::ZPG, 3));
+    opc_arr[0x06] = Some(OpMode(Inst::ASL, AddressMode::ZPG, 0));
+    opc_arr[0x08] = Some(OpMode(Inst::Php, AddressMode::IMPL, 0));
+    opc_arr[0x09] = Some(OpMode(Inst::Ora, AddressMode::IMM, 2));
+    opc_arr[0x0A] = Some(OpMode(Inst::ASL, AddressMode::ACC, 0));
+    opc_arr[0x0B] = None;
+    opc_arr[0x0D] = Some(OpMode(Inst::Ora, AddressMode::ABS, 4));
+    opc_arr[0x0E] = Some(OpMode(Inst::ASL, AddressMode::ABS, 0));
+
+    opc_arr
+}
 
 pub const fn get_op_mode(opc: u8) -> Option<OpMode> {
     OPS[opc as usize]
