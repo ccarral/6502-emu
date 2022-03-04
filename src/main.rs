6d881061c8f16c6dc54cf557e5fb9f36c14ec10d
Carlos Carral C <carloscarral13@gmail.com>
2022-03-04 10:23:10 -0600

    feature: basic cli args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-fn main() {
+pub fn main() {
+    use clap::{Arg, ArgGroup, Command};
     use mini6502::cpu::Cpu;
     use mini6502::memory::SimpleMemory;
+
+    let matches = Command::new("mini6502")
+        .author("Carlos Carral")
+        .arg(Arg::new("asm").long("asm").short('a').value_name("FILE"))
+        .arg(Arg::new("bin").long("bin").short('b').value_name("FILE"))
+        .group(
+            ArgGroup::new("input")
+                .args(&["asm", "bin"])
+                .required(true)
+                .multiple(false),
+        )
+        .get_matches();
+
+    let (input_file, is_asm) = {
+        if let Some(asm_file) = matches.value_of("asm") {
+            (asm_file, true)
+        } else if let Some(bin_file) = matches.value_of("bin") {
+            (bin_file, false)
+        } else {
+            unreachable!();
+        }
+    };
 
     let mem = mini6502::util::new_mem_with_asm("ORA #$20\nORA#$45").unwrap();
     let cpu = Cpu::with_mem(mem);
@@ -13,5 +36,5 @@ fn main() {
         stdin.read_line(&mut buf).unwrap();
     };
 
-    cpu.run(&mut callable).unwrap();
+    // cpu.run(&mut callable).unwrap();
 }
